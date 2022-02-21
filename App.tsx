cafb9bbf3386bca2dfca2519acb5504200342ab9
Bruno Lima <francisco.bruno@armateus.com.br>
2022-02-21 20:05:14 -0300

    estilizando a tela de login

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import theme from "./src/global/styles/theme";
 import { AppRoutes } from "./src/routes/app.routes";
 import { StatusBar } from "react-native";
+import { SignIn } from "./src/screens/SignIn";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -31,7 +32,8 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle="light-content" />
-        <AppRoutes />
+        <SignIn />
+        {/* <AppRoutes /> */}
       </NavigationContainer>
     </ThemeProvider>
   );
